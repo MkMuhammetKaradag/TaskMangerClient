cd907dfc9db34cc36b078ab3613d37eeffe833a8
MkMuhammetKaradag <mk.muhammetkaradag@gmail.com>
2024-10-16 10:56:41 +0300

    feat: Add Apollo Client setup with WebSocket and HTTP links
    - Configured Apollo Client with WebSocketLink for subscriptions and HttpLink for queries/mutations.
    - Added error handling using Apollo's onError link to log GraphQL and network errors.
    - Implemented split link to use WebSocket for subscriptions and HTTP for other operations.
    - Configured cache with type policies to merge incoming data for getMessages.
    - Included dev tools for better error and development messages.

---
+++ src/graphql/apolloClient.ts
@@ -0,0 +1,66 @@
+import { onError } from '@apollo/client/link/error';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { InMemoryCache } from '@apollo/client/cache';
+import { ApolloClient, ApolloLink, HttpLink, split } from '@apollo/client/core';
+import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+loadDevMessages();
+loadErrorMessages();
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:3000/graphql',
+});
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3000/graphql',
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) => {
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      );
+    });
+
+    if (networkError) {
+      console.log(`[Network error]: ${networkError}`);
+    }
+  }
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  ApolloLink.from([errorLink, httpLink])
+);
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        getMessages: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  link: splitLink,
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  cache,
+});
+export default client;
